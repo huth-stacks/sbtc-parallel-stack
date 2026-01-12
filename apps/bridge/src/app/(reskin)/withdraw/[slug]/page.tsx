5c90fae95d42f14bfaeb9c798101cde11e60b9c7
huth-stacks <230392931+huth-stacks@users.noreply.github.com>
2026-01-12 02:58:35 -0800

    Remove orphaned withdraw/[slug] files with broken imports
    These files imported non-existent stepper components from an old
    implementation that was replaced by the inline withdraw form.

--- apps/bridge/src/app/(reskin)/withdraw/[slug]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { StatusClient } from "./status-client";
-
-export default function Page() {
-  return (
-    <div className="flex flex-col md:flex-row items-center justify-center flex-1 py-6">
-      <StatusClient />
-    </div>
-  );
-}
